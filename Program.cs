ab767e645acfb0c0aa46236ca94cf7c002a508db
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-18 22:15:52 +0100

    Created Program.cs including Swagger

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
+using Authentication_Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AuthenticationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Authentication Api",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Authentication Api");
+    c.RoutePrefix = string.Empty;
+});
+app.Run();
