d04430cf08078f094df16d0f0903489d04323062
Erik Jonsson <erik.alfred.jonsson@gmail.com>
2026-01-18 22:04:49 +0100

    Added my database user entity.

---
+++ Data/Models/User.cs
@@ -0,0 +1,15 @@
+namespace Authentication_Api.Data.Models
+{
+    public class User
+    {
+        public string User_Name { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+    }
+}
